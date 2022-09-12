69ad48d6fb1117a1eb6772f97f7b66e3cc48b42c
yeahyouknowme <forrest.yeast@gmail.com>
2022-09-12 15:04:02 -0700

    animation timing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { markdown } from '@codemirror/lang-markdown';
 import { vim } from '@replit/codemirror-vim';
 import { marked } from 'marked';
 import { Icon } from '@mdi/react'
-import { mdiChevronUpCircle, mdiChevronDownCircle } from '@mdi/js';
+import { mdiChevronUpCircle, mdiChevronDownCircle, mdiMenu, mdiDotsHorizontal, mdiDownload } from '@mdi/js';
 import { useAtom } from 'jotai';
 
 import './App.css';
@@ -48,32 +48,50 @@ const App = () =>{
     setShowPreview(!showPreview);
   }
 
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    console.log("toggle menu");
+  }
+
   return (
     <div className="app-body">
-      <div className={ showPreview ? "editor-panel-hidden" : "editor-panel"}>
+      <div className={ showPreview ? "editor-panel-hidden" : "editor-panel" }>
         <div className="editor-header">
+          <div className="hamburger-button">
+            <Icon 
+              path={ mdiMenu }
+              size={ 1.2 }
+              onClick={ toggleMenu }
+              color="#D1D1FF"
+            />
+          </div>
         </div>
         <CodeMirror
-          value={defaultText}
-          theme={CMTheme}
-          extensions={[vim(), markdown()]}
+          value={ defaultText }
+          theme={ CMTheme }
+          extensions={ [vim(), markdown()] }
           width="100%"
-          onChange={onChange}
+          onChange={ onChange }
           id="editor"
         />
       </div>
       <div className={ showPreview ? "preview-panel" : "preview-panel-hidden" }>
           <div className="preview-header">
+            <Icon 
+              path={ mdiMenu }
+              size={ 1 }
+              onClick={ toggleMenu }
+            />
           </div>
           <div id="preview" dangerouslySetInnerHTML={{__html: cleanMarkup}} />
       </div>
       { screenSize > 600 
         ? null 
         : <div className={ showPreview ? "close-preview" : "open-preview" }>
-            <Icon 
-              path={ showPreview ? mdiChevronDownCircle : mdiChevronUpCircle}
-              size={2}
-              onClick={togglePreview}
+            <Icon
+              path={ showPreview ? mdiChevronDownCircle : mdiChevronUpCircle }
+              size={ 1.2 }
+              onClick={ togglePreview }
             />
           </div>
       }
